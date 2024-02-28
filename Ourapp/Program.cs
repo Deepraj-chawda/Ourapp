6cefc8073b0eb08a4e67b2b25fd0c7c3de5b9a20
Deepraj chawda <deeprajchawda@gmail.com>
2024-02-28 17:01:08 +0530

    Add project files.

---
+++ Ourapp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ourapp;
+
+Console.WriteLine("Hello, World!");
+string name;
+string password;
+Console.WriteLine("enter user name : ");
+name = Console.ReadLine();
+Console.WriteLine("enter password");
+password = Console.ReadLine();
+
+Console.WriteLine(UserAuthentication.UserLogin(name, password));
+
---
+++ Ourapp/UserAuthentication.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ourapp
+{
+    public class UserAuthentication
+    {
+        public static string UserLogin(string username, string password)
+        {
+            
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return "Please Provide username and password first";
+
+            }
+            if((username == "sam1256") || (password == "sam@1256@1256")) {
+                return "Login Success";
+            }
+            else
+            {
+                return "Login Fail";
+            }
+
+        }
+ 
+
+    }
+}
